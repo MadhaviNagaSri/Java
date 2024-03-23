857e6de2740d64959b3abf71e5595589f46c11cf
MadhaviNagaSri <147925584+MadhaviNagaSri@users.noreply.github.com>
2024-03-23 10:33:02 +0530

    Create array2.java

---
+++ array2.java
@@ -0,0 +1,14 @@
+// finding the EVEN numbers in the array
+class HelloWorld {
+    public static void main(String[] args) {
+     //initialaization of array elements
+     int a[] = {20,25,30,35,40,45,50,55,60,80,100};
+      for(int i:a){
+          
+          if(i%2==0){
+              System.out.println("The Even numbers are:"+ i);
+          }
+      }
+    
+    }
+}
