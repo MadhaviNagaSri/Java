16a973dbc7830a1ce57dce0ceaa9227a63d47b38
MadhaviNagaSri <147925584+MadhaviNagaSri@users.noreply.github.com>
2024-03-23 10:47:38 +0530

    Update array2.java

--- array2.java
+++ array2.java
@@ -3,6 +3,8 @@ class HelloWorld {
     public static void main(String[] args) {
      //initialaization of array elements
      int a[] = {20,25,30,35,40,45,50,55,60,80,100};
+        int sum=0;
+    System.out.println(".......................................");
       for(int i:a){
           
           if(i%2==0){
@@ -13,10 +15,16 @@ class HelloWorld {
               System.out.println("The odd numbers are:"+i);
           }*/
       }
+    System.out.println("...........................................");
       for(int i:a){
      if(i%2!=0){
               System.out.println("The odd numbers are:"+i);
           }
       }
+System.out.println("......................................");
+for(int i:a){
+    sum=sum+i;
+}
+   System.out.println("The sum of elements is:"+sum);
     }
 }
